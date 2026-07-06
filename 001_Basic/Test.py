ee3de2fdd46a18b6751c0cc79afb1bea94f1cb84
chintan <chintan@gmail.com>
2026-07-06 12:22:23 +0530

    all

---
+++ 001_Basic/Test.py
@@ -0,0 +1,37 @@
+# print("Hello python")
+
+# print('python is simple lang.')
+
+# print("""Hello 
+#       ghfhg
+#       hh
+#       python""")
+
+# hello \" python
+
+# print('hello " python')
+# print("hello ' python")
+
+# print("hel\blo \\\" py\nth\ton")
+
+# print("Hello",end=" ")
+# print("python")
+
+# print("java","python","node","php",sep=" | ")
+
+
+name = "hasib"
+age = 24
+
+# print("my name is {1} and age is {0}".format(name,age))
+
+# print(f"my name is {name} and age is {age}")
+
+
+# print(r"hel\blo \\\" py\nth\ton")
+
+# '',"",""""""
+# end ,sep
+# f,r
+# escaping
+
