bd5fb4150169b3d5761da116ca39989ef1804763
chintan <chintan@gmail.com>
2026-07-28 15:53:45 +0530

    string

---
+++ 002_Logical/pattern.py
@@ -0,0 +1,129 @@
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# for j in range(7):
+#     for i in range(15):
+#         print("*",end="")
+#     print()
+
+# for j in range(5):
+#     print(7*"*")
+   
+   
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+# for i in range(5):
+#     print((i+1)*"*")
+   
+# *****
+# ****
+# ***
+# **
+# *
+
+# for i in range(5):
+#     for j in range(5-i):
+#         print("*",end="")
+#     print()
+
+# for i in range(5):
+#     print("*"*(5-i))
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+   
+# lines=9
+# for i in range(lines):
+#     for k in range((lines-1)-i):
+#             print(" ",end="")
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+
+# lines=9
+# for i in range(lines):
+#     for k in range((lines-1)-i):
+#             print(" ",end="")
+#     for j in range(i+1):
+#         print("* ",end="")
+#     print()
+
+
+    #   *
+    #  * *
+    # * * *
+    #  * *
+    #   *
+    
+        #   *
+        #  * *
+        # *   *
+        #  * *
+        #   *
+    
+    
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(j+1,end="")
+#     print()
+
+# 5
+# 45
+# 345
+# 2345
+# 12345
+
+# 0
+# 10
+# 010
+# 1010
+# 01010
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print((i+j)%2,end="")
+#     print()
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         if i%2==j%2:
+#             print("0",end="")
+#         else:
+#             print("1",end="")
+#     print()
+
---
+++ 003_String/demo.py
@@ -0,0 +1,41 @@
+# st = "sun Rises in East ß"
+
+# print(len(st))
+# print(st.lower())
+# print(st.casefold())
+
+# print(st.capitalize())
+# print(st.upper())
+# print(st.title())
+
+# print(st)
+# print(st.strip())
+
+
+# print("abc".isalpha())
+# print("123".isdigit())
+# print("ass123$".isalnum())
+
+
+# print(st.replace("s","T",2))
+# print(st.startswith("s"))
+# print(st.endswith("t"))
+
+# print(st.find("ini"))
+
+# print(st.split(" "))
+
+# print("xyz".join("abc"))
+
+# print("abc".zfill(4))
+# print("abc".center(11,"*"))
+
+
+st = "sun Rises in East ß"
+
+# print(st[1])
+# print(st[5:])
+# print(st[:5])
+# print(st[2:9:2])
+# print(st[-4:-1])
+print(st[::-1])
