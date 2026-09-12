76732e9a2b8e720a03c986abb40d4b92ef2f8711
chintan <chintan@gmail.com>
2026-09-12 15:58:20 +0530

    ALL

--- 013_Django/dbproject/myapp/views.py
+++ 013_Django/dbproject/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from myapp.models import *
 
 # Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def register(request):
+    data = request.POST
+    name = data.get("name")
+    email = data.get("email")
+    age = data.get("age")
+    Student.objects.create(name=name,email=email,age=age)
+    return render(request,"index.html",{"msg":"Registration success"})
+
+def display(request):
+    students = Student.objects.all()
+    return render(request,"display.html",{"students":students})
