ddbd986c88bb935fa6d3c10a86f93df1029c15a6
chintan <chintan@gmail.com>
2026-09-15 15:38:02 +0530

    django

--- 013_Django/dbproject/myapp/views.py
+++ 013_Django/dbproject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from myapp.models import *
 
 # Create your views here.
@@ -7,12 +7,34 @@ def index(request):
 
 def register(request):
     data = request.POST
+    id = data.get("id")
     name = data.get("name")
     email = data.get("email")
     age = data.get("age")
-    Student.objects.create(name=name,email=email,age=age)
-    return render(request,"index.html",{"msg":"Registration success"})
+    
+    if id:
+        st = Student.objects.get(pk=id)
+        st.name = name
+        st.email = email
+        st.age = age
+        st.save()
+        return render(request,"index.html",{"msg":"Update success"})
+        
+    else:
+        Student.objects.create(name=name,email=email,age=age)
+        return render(request,"index.html",{"msg":"Registration success"})
 
 def display(request):
     students = Student.objects.all()
     return render(request,"display.html",{"students":students})
+
+def delete_student(request):
+    id = request.GET.get("id")
+    st = Student.objects.get(id=id)
+    st.delete()
+    return redirect("display")
+
+def update_student(request):
+    id = request.GET.get("id")
+    st = Student.objects.get(id=id)
+    return render(request,"index.html",{"st":st})
